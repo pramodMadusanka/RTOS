dce8aaa4705cf79f870f4ea913df4faddceff2cc
pramodMadusanka <68587794+pramodMadusanka@users.noreply.github.com>
2020-07-27 19:29:39 +0530

    Update Scheduler.h

--- Scheduler/Cyclic Scheduler/Scheduler.h
+++ Scheduler/Cyclic Scheduler/Scheduler.h
@@ -1,19 +1,18 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
 #include <Arduino.h>
 #include <stdio.h>
+#include <Queue.h>
 
 class Scheduler{
 	public:
-		Scheduler(void);
-		char* schedule(int period[], int frame_size, int no_of_tasks);
-		Queue PCB;
+		node** calcSchedule(int major_cycle, float frame_size, int min_period);
+		node** schedule;
+		Queue queue;
+
 	private:
-		int gcd(int a, int b);
-		int LCM(int arr[], int n);
-		int major_cycle;
-		int no_of_frames;
+
 };
 
 #endif
