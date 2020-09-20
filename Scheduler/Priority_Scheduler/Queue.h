8a5c89785c8c9fa0bcbd61aba82dd7975fda2458
pramodMadusanka <68587794+pramodMadusanka@users.noreply.github.com>
2020-09-20 21:00:53 +0530

    Create Queue.h

---
+++ Scheduler/Priority_Scheduler/Queue.h
@@ -0,0 +1,36 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <Arduino.h>
+#include <stdio.h>
+
+typedef struct strnode{
+	char name;
+	void (*taskptr)(void* arg);
+	uint8_t priority;
+	int wakeupTime;
+	struct strnode* next;
+}node;
+
+class Queue{
+	public:
+		Queue(void);
+		struct strnode* start;
+		struct strnode* end;
+		void addTask(char name, void (*taskptr)(void* arg), uint8_t priority);
+		void enqueue(struct strnode* node, int wakeupTime);
+		struct strnode* dequeue();
+		void printQueue();
+		void quickSort(struct strnode** headRef);
+		struct strnode* getStart();
+		void addNode(struct strnode* head, struct strnode* node);
+		
+	private:
+		struct strnode* tmp;
+		struct strnode* createNode(char name, void (*taskptr)(void* arg), uint8_t priority, int wakeupTime);
+		struct strnode* getTail(struct strnode* cur);
+		struct strnode* partition(struct strnode *head, struct strnode *end, struct strnode **newHead, struct strnode **newEnd);
+		struct strnode* quickSortRecur(struct strnode *head, struct strnode *end);
+};
+
+#endif
