ac81fb34cc4a09f558be9f9fd16f96875cd22c0b
pramodMadusanka <68587794+pramodMadusanka@users.noreply.github.com>
2020-09-27 20:23:45 +0530

    Update Queue.h

--- Scheduler/Queue/Queue.h
+++ Scheduler/Queue/Queue.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 
 typedef struct node{
-	char name;
 	int period;
 	uint8_t exec_time;
 	void (*taskptr)(void* arg);
@@ -16,8 +15,8 @@ typedef struct node{
 class Queue{
 	public:
 		Queue(void);
-		void addTask(char name, int period, uint8_t exec_time, void (*taskptr)(void* arg));
-		void enqueue(char name, int period, uint8_t exec_time, void (*taskptr)(void* arg), uint8_t checked);
+		void addTask(int period, uint8_t exec_time, void (*taskptr)(void* arg));
+		void enqueue(int period, uint8_t exec_time, void (*taskptr)(void* arg), uint8_t checked);
 		struct node* dequeue();
 		int getMajorCycle();
 		int getMinPeriod();
@@ -26,7 +25,7 @@ class Queue{
 		struct node* tmp;
 		struct node* start;
 		struct node* end;
-		struct node* createNode(char name, int period, uint8_t exec_time, void (*taskptr)(void* arg), uint8_t checked);
+		struct node* createNode(int period, uint8_t exec_time, void (*taskptr)(void* arg), uint8_t checked);
 		int* getPeriods();
 		uint8_t getNoOfTasks();
 		int GCD(int a, int b);
